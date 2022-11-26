0e01e3294d7f39a67ff883340f02a7ff41815960
Akinsoji Richard <111376893+Y0ungDevel0per@users.noreply.github.com>
2022-11-26 12:13:45 +0100

    new push

---
+++ flicker/src/Requests.js
@@ -0,0 +1,11 @@
+const key = '660527c3e2b9bf5e9855a01129ad812c'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+export default requests
